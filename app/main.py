0165c9aae22c8a3892879e4b651dd97121591181
Radoslaw Ptak <radek1026@interi.pl>
2025-08-21 14:53:49 +0200

    Improvement py-person-class

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class Person:
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people: list[dict]) -> list[Person]:
 
     Person.people.clear()
     person_list = [Person(person["name"]
@@ -15,10 +15,12 @@ def create_person_list(people: list) -> list:
 
     for person_dict in people:
 
-        if person_dict.get("wife") is not None:
+        wife_name = person_dict.get("wife")
+        if wife_name is not None and wife_name in Person.people:
             Person.people[person_dict["name"]].wife \
                 = Person.people[person_dict["wife"]]
-        if person_dict.get("husband") is not None:
+        husband_name = person_dict.get("husband")
+        if husband_name is not None and husband_name in Person.people:
             Person.people[person_dict["name"]].husband \
                 = Person.people[person_dict["husband"]]
     return person_list
